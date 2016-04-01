2ab7f76462e16694fc89d602948ba0b6e94519b2
Hillary Hintzen <hillary07@gmail.com>
2016-04-01 09:58:16 -0700

    create answer model

---
+++ app/models/answer.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  id: DS.attr(),
+  user: DS.attr(),
+  text: DS.attr()
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+var answers = [{
+  id: 1,
+  user: "Your mom",
+  text: "You should do X, Y, or Z"
+}, {
+  d: 1,
+  user: "cat",
+  text: "meow meow"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return answers;
+  }
 });
